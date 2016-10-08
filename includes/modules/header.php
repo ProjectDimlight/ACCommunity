103ad5fed7792f2d50be7e83bfc1b1c5d687e883
ProjectDimlight <1846326091@qq.com>
2016-10-08 13:09:28 +0800

    Update header.php

--- includes/modules/header.php
+++ includes/modules/header.php
@@ -1,13 +1,9 @@
 <?php session_start(); ?>
 
-<<<<<<< HEAD
 //QSC: 172.20.217.62
 //Mobile: 192.168.43.3
-
-<?php $hostip = 'localhost'; $hostport = 8080; $title = '寂月城' ?>
-=======
+           
 <?php $hostip = '191.101.11.174'; $hostport = 80; $title = '寂月城' ?>
->>>>>>> 285b1e10ab59fe0107661a3b94804118a93f3b74
 
 <html lang='zh'>
     <meta charset="utf-8"/>
